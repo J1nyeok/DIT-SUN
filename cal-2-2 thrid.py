132ca7b64e677fe5de07cf3d2ed901bbc48bb0da
J1nyeok <100898106+J1nyeok@users.noreply.github.com>
2022-03-08 15:41:47 +0900

    Add files via upload

---
+++ cal-2-2 thrid.py
@@ -0,0 +1,19 @@
+def add(a, b) :
+    c = a + b
+    return c
+
+def minus(a, b):
+    c = a - b
+    return c
+
+def multi(a, b):
+    c = a * b
+    return c
+
+def divide(a, b):
+    c = a / b
+    return c
+    
+sum = add,minus,multi,divide(10, 20) :
+print("sum =", sum)
+
